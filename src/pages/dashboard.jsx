35414504e287867965c3796055adb34b6176b345
Daniel-henr <danielhpp05@gmail.com>
2024-09-29 03:15:15 -0300

    1 problem resolved

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
     <div>
       <h1 className='text-primary-950  text-4xl font-semibold ml-4 mt-2'>Dashboard</h1>
       <div className=' min-w-custom-66 min-h-custom-35 p-3 bg-essencial-100 border-essencial-200 border-2 rounded-2xl ml-4'>
-        <div className='flex grid grid-cols-4 '>
+        <div className='grid grid-cols-4 '>
             <Balance />
             <Income />
             <Costs />
