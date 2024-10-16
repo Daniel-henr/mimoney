3ef4af2a350f367c5d8754752c959bb6cdb5be6c
Daniel-henr <danielhpp05@gmail.com>
2024-10-16 18:55:47 -0300

    The History component has received some adjustments.

--- src/components/historyTab.jsx
+++ src/components/historyTab.jsx
@@ -4,21 +4,17 @@ const formatCurrency = (value) => {
     return value.toLocaleString('pt-BR', {
       style: 'currency',
       currency: 'BRL',
-    }).replace('R$', '').trim()
+    })
   };
 
 const HistoryTab = () => {
     const { transactions } = useTransactions()
 
-    const sortedTransactions = [...transactions].sort((a, b) => {
-        const dateA = new Date(`${a.Date} ${a.time}`)
-        const dateB = new Date(`${b.Date} ${b.time}`)
-        return dateA - dateB
-    })
+    const reversedTransactions = [...transactions].reverse()
 
     return (
         <div>
-                {sortedTransactions.map((transaction, index) => (
+                {reversedTransactions.map((transaction, index) => (
                     <div key={index} className='py-2.5 whitespace-nowrap border-b-2 border-essencial-200 flex justify-between items-center text-support-800 font-semibold dark:text-essencial-200 dark:border-support-700'>
                         <div className='w-80 flex justify-center'>
                         <h3>{transaction.description}</h3>                            
